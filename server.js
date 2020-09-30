4d4b0566a7f4c31107fc3a24c5b00ea88a5c77b9
Jonathan Lemman <jonathan.lemman@gmail.com>
2020-09-29 23:22:50 -0500

    tested mongodb connection and added models folder

--- server.js
+++ server.js
@@ -14,3 +14,7 @@ const db = require('./config/keys.js').mongoURI;
 mongoose.connect(db)
     .then(() => console.log('Mongo Connected...'))
     .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
